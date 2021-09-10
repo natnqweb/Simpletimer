c05acf11b3c5851185e96af1bd81087f4a83f337
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2021-09-10 22:10:24 +0200

    Add files via upload

--- src/Simpletimer.cpp
+++ src/Simpletimer.cpp
@@ -2,9 +2,9 @@
 
     bool Simpletimer::timer(unsigned long waitTime)//manage your tasks enter time then timer function will return true every given time, remember that it returns true at the beggining of program
     {
-        if (millis() - before >= waitTime)
+        if (micros() - before >= waitTime*1000U)
         {
-            before = millis();
+            before = micros();
             return true;
         }
         else
