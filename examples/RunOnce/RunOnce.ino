5613b548848310f8dc5f0115e08a3bd16aae4a84
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2021-12-12 14:15:22 +0100

    Add files via upload

---
+++ examples/RunOnce/RunOnce.ino
@@ -0,0 +1,41 @@
+#include "Simpletimer.h"
+// callback function cant take anything and return anything
+
+Simpletimer::RunOnce Once;
+int i=1;
+void callback1(){
+  Serial.println(F("entry once"));
+  i++;
+}
+
+void callback2(){
+	 Serial.println(F("other method"));
+	 i++;
+	 Serial.println("number of tasks performed : "+String(i));
+}
+
+void setup(){
+	
+Serial.begin(115200);
+
+
+
+}
+void loop(){
+	//static Simpletimer::RunOnce DoSomething([&](){ here goes your code that need to run once});
+	// perform  task once in loop
+	static Simpletimer::RunOnce DoSomething;
+	DoSomething.Run([&](){callback1(); Serial.println(F("you can place here anything "));});
+	// reset previous task in anotherone and then it can perform second time
+	static Simpletimer::RunOnce reset_previous_task;
+	reset_previous_task.Run([&](){DoSomething.Reset(); Serial.println("task 1 RESET being called now");});
+	//another way to perform task once you can always reset calling Once.Reset();
+	Once.Run(callback2);
+	
+
+
+
+	
+
+
+}
